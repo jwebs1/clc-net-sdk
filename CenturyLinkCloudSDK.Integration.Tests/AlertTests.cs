2a43945a4afa2c06ff64a31015d61767226950fd
Mario Mamalis <mario.mamalis@gmail.com>
2015-02-25 10:32:54 -0600

    Added AlertService and method to get account alert policies

---
+++ CenturyLinkCloudSDK.Integration.Tests/AlertTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CenturyLinkCloudSDK.ServiceModels;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CenturyLinkCloudSDK.Integration.Tests
+{
+    [TestClass]
+    public class AlertTests
+    {
+        private static Client client;
+        private static Authentication authentication;
+
+        [ClassInitialize]
+        public static void Login(TestContext testContext)
+        {
+            client = new Client("mario.mamalis", "MarioTest!");
+            authentication = client.Authentication;
+        }
+
+        [TestMethod]
+        public async Task GetAlertPoliciesForAccountReturnValidData()
+        {
+            var alertPolicies = await client.Alerts.GetAlertPoliciesForAccount().ConfigureAwait(false);
+            Assert.IsTrue(alertPolicies.Items.ToList().Count > 0);
+        }
+    }
+}
